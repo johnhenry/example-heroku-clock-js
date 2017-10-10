d488f0c5de06e723d408a7acd77ff214fec24590
Jason Cooke <Jason-Cooke@users.noreply.github.com>
2017-10-10 18:46:02 +1300

    Format comments

--- clock.js
+++ clock.js
@@ -1,16 +1,16 @@
-//OnInterval
+// OnInterval
 var threeSecondInterval = function(){
     console.log("Another 3 seconds have gone by. What did you do in them?");
 }
 setInterval(threeSecondInterval, 3000)
 
-//For specific times, use a chron job
+// For specific times, use a chron job
 var fifteenSeconsAfterMinute = function() {
   console.log("Another minute is gone forever. Hopefully, you made the most of it...");
 }
 var CronJob = require('cron').CronJob;
 new CronJob({
-  cronTime: "15 * * * * *",//15 seconds after every minute
+  cronTime: "15 * * * * *", // 15 seconds after every minute
   onTick: fifteenSeconsAfterMinute,
   start: true,
   timeZone: "America/Los_Angeles"
